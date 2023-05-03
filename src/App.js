ee608c8e8323ac3f3931fccdaf4b672aa2e2986e
Sarah Andyshak <sarah.andyshak@gmail.com>
2023-05-03 16:56:44 -0700

    Add useReducer branch to practice with useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Counter from './Counter';
-import Timer from './Timer';
 
 function App() {
   return (
     <div className="App">
       <Counter />
-      <Timer />
     </div>
   );
 }
--- src/Counter.js
+++ src/Counter.js
@@ -1,18 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
+
+const initialState = {
+  counter: 0
+}
+
+function reducer(state, action) {
+  switch(action.type){
+    case 'increment':
+      return{
+        counter: state.counter + 1
+      };
+    default:
+      throw new Error(`There is no action matching ${action.type}.`);
+  }
+}
 
 function Counter() {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [hidden, setHidden] = useState(false);
 
   useEffect(() => {
-    console.log("effect!");
-    document.title = counter;
-  }, [counter]);
+    document.title = state.counter;
+  }, [state.counter]);
 
-  return(
+  return (
     <React.Fragment>
-      {hidden ? <h1>Count Hidden</h1> : <h1>{counter}</h1>}
-      <button onClick={() => setCounter(counter + 1)}>Count!</button>
+      {hidden ? <h1>Count Hidden</h1> : <h1>{state.counter}</h1>}
+      <button onClick={() => dispatch({type: 'increment'})}>Count!</button>
       <button onClick={() => setHidden(!hidden)}>Hide/Show</button>
     </React.Fragment>
   );
